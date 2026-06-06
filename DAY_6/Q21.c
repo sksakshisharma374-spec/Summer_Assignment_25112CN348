c0aa68424af46f152ef1c0b12b3899c61e8ac579
Sakshi <sksakshisharma374@gmail.com>
2026-06-06 21:42:04 +0530

    Add DAY_6 solutions

---
+++ DAY_6/Q21.c
@@ -0,0 +1,23 @@
+//CONVERT DECIMAL TO BINARY
+#include <stdio.h>
+
+int main() {
+    int num, binary[32], i = 0;
+
+    printf("Enter a decimal number: ");
+    scanf("%d", &num);
+
+    while (num > 0) {
+        binary[i] = num % 2;
+        num = num / 2;
+        i++;
+    }
+
+    printf("Binary number = ");
+
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%d", binary[j]);
+    }
+
+    return 0;
+}
