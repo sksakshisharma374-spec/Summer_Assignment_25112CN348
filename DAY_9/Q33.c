8fc2d3527d595698567017ed913aaf22b21c1f89
Sakshi <sksakshisharma374@gmail.com>
2026-06-08 13:28:39 +0530

    Add DAY_8 and DAY_9 assignments

---
+++ DAY_9/Q33.c
@@ -0,0 +1,18 @@
+//REVERSE STAR PATTERN
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for(i = 5; i > 0; i--)
+    {
+        for(j = 0; j < i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
