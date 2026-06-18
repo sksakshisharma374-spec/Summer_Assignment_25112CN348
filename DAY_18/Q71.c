4ce5a61453d918be7bd37a0410cff2e8d32bba15
Sakshi <sksakshisharma374@gmail.com>
2026-06-18 18:24:24 +0530

    Add DAY_18 assignments

---
+++ DAY_18/Q71.c
@@ -0,0 +1,40 @@
+//Binary Search
+#include <stdio.h>
+
+int main() {
+    int n, i, key;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter sorted elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    int low = 0, high = n - 1, mid;
+
+    while(low <= high) {
+        mid = (low + high) / 2;
+
+        if(arr[mid] == key) {
+            printf("Element found at index %d", mid);
+            return 0;
+        }
+        else if(arr[mid] < key) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    printf("Element not found");
+
+    return 0;
+}
