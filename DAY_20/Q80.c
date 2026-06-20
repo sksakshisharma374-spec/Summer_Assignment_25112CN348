0e4f09d2573085673a50fa9871d6e9c8f63467fc
Sakshi <sksakshisharma374@gmail.com>
2026-06-20 22:24:02 +0530

    Add DAY_20 solutions

---
+++ DAY_20/Q80.c
@@ -0,0 +1,31 @@
+//Find Column-wise Sum
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10];
+    int r, c, i, j, sum;
+
+    printf("Enter rows and columns: ");
+    scanf("%d%d",&r,&c);
+
+    printf("Enter matrix:\n");
+
+    for(i=0;i<r;i++)
+        for(j=0;j<c;j++)
+            scanf("%d",&a[i][j]);
+
+    printf("Column-wise Sum:\n");
+
+    for(j=0;j<c;j++)
+    {
+        sum=0;
+        for(i=0;i<r;i++)
+        {
+            sum=sum+a[i][j];
+        }
+        printf("Column %d = %d\n",j+1,sum);
+    }
+
+    return 0;
+}
