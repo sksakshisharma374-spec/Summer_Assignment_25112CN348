ef94084d0086fa24397d2092b84d67ded033813f
Sakshi <sksakshisharma374@gmail.com>
2026-06-23 18:49:25 +0530

    Add DAY_23 assignments

---
+++ DAY_23/Q92.c
@@ -0,0 +1,30 @@
+//Find Maximum Occurring Character
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int i, j, count, max = 0;
+    char ch;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        count = 0;
+
+        for(j = 0; str[j] != '\0'; j++) {
+            if(str[i] == str[j])
+                count++;
+        }
+
+        if(count > max) {
+            max = count;
+            ch = str[i];
+        }
+    }
+
+    printf("Maximum occurring character = %c", ch);
+
+    return 0;
+}
